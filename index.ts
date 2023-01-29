0ec7773508cc42100c8a87fe4ab3835e441985eb
Aleksandr Kariakin <aleksandr.kariakin@toptal.com>
2023-01-29 17:57:47 +0300

    Init

---
+++ index.ts
@@ -0,0 +1,17 @@
+import express, {
+  Express,
+  Request,
+  Response,
+} from 'express';
+import dotenv from 'dotenv';
+
+const app: Express = express();
+dotenv.config();
+
+const port = process.env.PORT;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Express + TypeScript Server');
+});
+
+app.listen(port);
